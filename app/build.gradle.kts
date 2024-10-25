be383a0b96f244ab205d747730156b81f50fa39d
Willian Santos <willjsan@motorola.com>
2024-10-25 13:51:05 -0300

    row, image, column, spacer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/bill/com/MainActivity.kt
+++ app/src/main/java/bill/com/MainActivity.kt
@@ -4,38 +4,60 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import bill.com.ui.theme.KotlinAndComposeTheme
 
 class MainActivity : ComponentActivity() {
+
+    data class Message(val a: String, val b: String)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             KotlinAndComposeTheme {
-                fistComposableFunction()
+                Surface (modifier = Modifier.fillMaxSize()){
+                    FirstComposableFunction(Message("Android", "Jetpack Compose"))
+                }
+                //fistComposableFunction("From Compose")
             }
         }
     }
 
     @Composable
-    fun fistComposableFunction() {
-        Column() {
-            Text(
-                text = "Hello World from Compose"
+    fun FirstComposableFunction(msg: Message) {
+        Row(modifier = Modifier.padding(start = 20.dp, top = 40.dp, bottom = 40.dp, end = 20.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_playstore),
+                contentDescription = "Icone do launcher",
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape)
             )
-            Text(
-                text = "sec line"
-            )
+            Spacer(modifier = Modifier.width(10.dp))
 
+            Column {
+                Text(text = "Hello")
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(text = "${msg.a} and ${msg.b}")
+            }
         }
     }
+
 }
